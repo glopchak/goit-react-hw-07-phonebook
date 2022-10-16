ad69a01a6f170566de63787984997805f5952091
Diana Glopchak <dglopchak@gmail.com>
2022-10-16 20:49:00 +0300

    finalization

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://634c382d317dc96a30940c28.mockapi.io/contacts';
+axios.defaults.baseURL = 'https://634c4308317dc96a3094d594.mockapi.io/contacts';
 
 export const fetchContacts = createAsyncThunk(
    'contacts/fetchAll',
